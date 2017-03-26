3532ea13d05c73589143ae1c8bc82b396eaef2d7
Vikram Peddinti <vikram.peddinti@gmail.com>
2017-03-25 18:42:05 -0700

    Movement Added Photon fully integrated

--- Assets/Scripts/Multiplayer/MP_PlayerManager.cs
+++ Assets/Scripts/Multiplayer/MP_PlayerManager.cs
@@ -15,6 +15,7 @@ public class MP_PlayerManager : MonoBehaviour {
             gameObject.GetComponent<Pl_MouseLook>().enabled = true;
             gameObject.GetComponent<Pl_Controller>().enabled = true;
             gameObject.GetComponent<InputManager>().enabled = true;
+            gameObject.GetComponent<PhotonTransformView>().enabled = true;
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(2).gameObject.SetActive(true);
 
