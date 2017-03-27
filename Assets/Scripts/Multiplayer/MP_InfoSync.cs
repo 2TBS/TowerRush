cc09b57e10aa51d05312f60a13e29062ba726b80
Vikram Peddinti <vikram.peddinti@gmail.com>
2017-03-26 20:29:07 -0700

    Player name

---
+++ Assets/Scripts/Multiplayer/MP_InfoSync.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+public class MP_InfoSync : Photon.MonoBehaviour {
+
+
+    public TextMesh nameText;
+
+    private PhotonPlayer _owner;
+
+	// Use this for initialization
+	void Start () {
+        _owner = this.photonView.owner;
+        nameText.text = _owner.NickName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
